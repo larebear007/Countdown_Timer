3963915327cfd31b3e051f8351d327f8c9d5a4a2
larebear007 <larencozart@gmail.com>
2022-04-21 15:31:09 -0500

    comment update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ print('Welcome to Timer Countdown!\n')
 
 print('This timer will count down by seconds')
 
-# Retrieving countdown #, while checking for numeric value & looping if not
+# Retrieving countdown #, while checking for numeric input & looping if not
 while True:
     number = input('Enter a whole number to countdown: ')
     if number.isnumeric():
