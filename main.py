63ca0eb836eedf0b0c9a964167a15d46d8bdf758
larebear007 <larencozart@gmail.com>
2022-04-21 15:30:02 -0500

    Bug fix: catch and loop for non-numeric inputs

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ import time
 print('Welcome to Timer Countdown!\n')
 
 print('This timer will count down by seconds')
-number = input('Enter a whole number to countdown: ')
-number = int(number)
-print('Starting Timer . . .')
-time.sleep(2)
 
+# Retrieving countdown #, while checking for numeric value & looping if not
+while True:
+    number = input('Enter a whole number to countdown: ')
+    if number.isnumeric():
+        number = int(number)
+        print('Starting Timer . . .')
+        time.sleep(2)
+        break
+    else:
+        print('Please enter a number for the countdown timer.')
+
+# Timer display
 while number > 0:
     print(number)
     time.sleep(1)
     number = number - 1
-print('TIMER ENDED')
+
+print('TIMER ENDED !')
